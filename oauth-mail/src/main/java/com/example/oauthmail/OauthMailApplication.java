905f42d430c31755ed0b335cf55c7c33dbdee975
kimjongha99 <jjhsun99@gmail.com>
2024-01-14 21:47:10 +0900

    oauth-mail프로젝트 초기 의존성

---
+++ oauth-mail/src/main/java/com/example/oauthmail/OauthMailApplication.java
@@ -0,0 +1,13 @@
+package com.example.oauthmail;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OauthMailApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(OauthMailApplication.class, args);
+    }
+
+}
---
+++ oauth-mail/src/test/java/com/example/oauthmail/OauthMailApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.oauthmail;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OauthMailApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
